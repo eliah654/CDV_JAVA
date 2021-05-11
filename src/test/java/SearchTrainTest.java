761d0e9a8846ed39c5533c636d42683aea84cfef
eliah654 <eliahbalard@gmail.com>
2021-05-11 18:47:20 +0200

    dayli commit

--- src/test/java/SearchTrainTest.java
+++ src/test/java/SearchTrainTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -11,6 +12,7 @@ import java.util.concurrent.TimeUnit;
 public class SearchTrainTest {
     public WebDriver driver;
 
+
     @BeforeClass
     public void setUp(){
         System.setProperty("webdriver.firefox.driver", "/geckodriver/geckodriver");
@@ -19,13 +21,14 @@ public class SearchTrainTest {
 
     @Test
     public void testSearchTrain(){
+
         driver.get("https://portalpasazera.pl");
         String pageTitle = driver.getTitle();
 
         SearchFormClicks cookieDismiss = new SearchFormClicks(driver);
 
         Assert.assertEquals(pageTitle,"Wyszukiwarka rozkładu jazdy pociągów PKP PLK S.A. - Portal Pasażera - PKP Polskie Linie Kolejowe S.A.");
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
         cookieDismiss.setCookieAgree();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
--- src/test/java/pages/SearchFormClicks.java
+++ src/test/java/pages/SearchFormClicks.java
@@ -1,23 +1,29 @@
 package pages;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class SearchFormClicks {
 
     WebDriver driver;
+    WebDriverWait wait;
 
-    @FindBy(xpath = "//button[@id='cookieman-save-btn']")
+    @FindBy(xpath = "//button[contains(text(),'Akceptuj wszystkie pliki cookie')]")
     WebElement cookieAgree;
 
     public SearchFormClicks(WebDriver driver){
         this.driver = driver;
         PageFactory.initElements(driver, this);
+
     }
 
     public void setCookieAgree() {
+        wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedCondition.elementToBeClickable(cookieAgree));
         cookieAgree.click();
     }
 }
