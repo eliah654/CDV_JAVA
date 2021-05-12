ee7511c8c1a5cd0352dd01d330fb041cbe561f76
eliah654 <eliahbalard@gmail.com>
2021-05-12 22:05:58 +0200

    another dayli commit 12.05

--- src/test/java/SearchTrainTest.java
+++ src/test/java/SearchTrainTest.java
@@ -1,45 +1,69 @@
 import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
-import org.testng.Assert;
+//import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pages.SearchFormClicks;
 
-import java.util.concurrent.TimeUnit;
-
 public class SearchTrainTest {
     public WebDriver driver;
 
+    public void slowDown(int howLong){
+
+        try{Thread.sleep(howLong);}
+        catch(InterruptedException e){System.out.println("fghdfd");}
+    }
 
     @BeforeClass
     public void setUp(){
         System.setProperty("webdriver.firefox.driver", "/geckodriver/geckodriver");
         driver = new FirefoxDriver();
+        //driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://portalpasazera.pl");
     }
 
     @Test
     public void testSearchTrain(){
 
-        driver.get("https://portalpasazera.pl");
-        String pageTitle = driver.getTitle();
+//        String pageTitle = driver.getTitle();
+//        Assert.assertEquals(pageTitle,"Wyszukiwarka rozkładu jazdy pociągów PKP PLK S.A. - Portal Pasażera - PKP Polskie Linie Kolejowe S.A.");
 
+        this.slowDown(1000);
         SearchFormClicks cookieDismiss = new SearchFormClicks(driver);
-
-        Assert.assertEquals(pageTitle,"Wyszukiwarka rozkładu jazdy pociągów PKP PLK S.A. - Portal Pasażera - PKP Polskie Linie Kolejowe S.A.");
-
         cookieDismiss.setCookieAgree();
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
+        this.slowDown(1000);
+        SearchFormClicks insertOdjazd = new SearchFormClicks(driver);
+        insertOdjazd.wyjazdInput(cellOdjazd);
 
+        this.slowDown(1000);
+        SearchFormClicks insertPrzyjazd = new SearchFormClicks(driver);
+        insertPrzyjazd.przyjazdInput(cellPrzyjazd);
 
+        this.slowDown(1000);
+        SearchFormClicks insertData = new SearchFormClicks(driver);
+        insertData.dataInput(cellData);
 
+        this.slowDown(1000);
+        SearchFormClicks insertGodzina = new SearchFormClicks(driver);
+        insertGodzina.godzinaInput(cellGodzina);
+
+        this.slowDown(1000);
+        SearchFormClicks clickSzukaj = new SearchFormClicks(driver);
+        clickSzukaj.searchButton();
+
+        //this.slowDown(8000);
 
     }
 
     @AfterClass
-    public void tearDown(){
+
+    public void tearDown()  {
+
+        this.slowDown(5000);
         driver.quit();
     }
 
--- src/test/java/pages/SearchFormClicks.java
+++ src/test/java/pages/SearchFormClicks.java
@@ -1,10 +1,12 @@
 package pages;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
 
 
 public class SearchFormClicks {
@@ -15,15 +17,108 @@ public class SearchFormClicks {
     @FindBy(xpath = "//button[contains(text(),'Akceptuj wszystkie pliki cookie')]")
     WebElement cookieAgree;
 
-    public SearchFormClicks(WebDriver driver){
+    @FindBy(xpath = "//button[@class='btn btn--lg btn-start-search txuc']")
+    WebElement searchButton;
+
+    @FindBy(xpath = "//input[@id='departureFrom']")
+    WebElement wyjazdInput;
+
+    @FindBy(xpath = "//input[@id='arrivalTo']")
+    WebElement przyjazdInput;
+
+    @FindBy(xpath = "//input[@id='main-search__dateStart']")
+    WebElement dataInput;
+
+    @FindBy(xpath = "//input[@id='main-search__timeStart']")
+    WebElement godzinaInput;
+
+    @FindBy(xpath = "//span[@class='label-inside txlc']")
+    WebElement directConnections;
+
+    @FindBy(xpath = "//button[contains(text(),'Więcej opcji wyszukiwania')]")
+    WebElement moreSearchOptions;
+
+    @FindBy(xpath = "//div[@class='main-search__options-close']//button[@type='button']//span[@class='txlc'][contains(text(),'Zamknij opcje wyszukiwania')]")
+    WebElement closeSearchOptions;
+
+    public SearchFormClicks(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
-
     }
 
-    public void setCookieAgree() {
-        wait = new WebDriverWait(driver,10);
-        wait.until(ExpectedCondition.elementToBeClickable(cookieAgree));
+    public void ClearField(WebElement pageElement){
+        pageElement.sendKeys(Keys.CONTROL, "a");
+        pageElement.sendKeys(Keys.BACK_SPACE);
+    }
+
+    public void setCookieAgree(){
+
+        wait = new WebDriverWait(driver, 10);
+        wait.until(elementToBeClickable(cookieAgree));
         cookieAgree.click();
+    }
+
+
+    public void searchButton(){
+
+        wait = new WebDriverWait(driver, 10);
+        wait.until(elementToBeClickable(searchButton));
+        searchButton.click();
+    }
+
+    public void wyjazdInput(String insertTown){
+
+        wait = new WebDriverWait(driver, 10);
+        wait.until(elementToBeClickable(wyjazdInput));
+        wyjazdInput.click();
+        wyjazdInput.sendKeys(insertTown);
+    }
+
+    public void przyjazdInput(String insertTown){
+
+        wait = new WebDriverWait(driver, 10);
+        wait.until(elementToBeClickable(przyjazdInput));
+        przyjazdInput.click();
+        przyjazdInput.sendKeys(insertTown);
+    }
+
+
+    public void dataInput(String insertData){
+
+        wait = new WebDriverWait(driver, 10);
+        wait.until(elementToBeClickable(dataInput));
+        dataInput.click();
+        this.ClearField(dataInput);
+        dataInput.sendKeys(insertData);
+    }
+
+    public void godzinaInput(String insertGodzina){
+
+        wait = new WebDriverWait(driver, 10);
+        wait.until(elementToBeClickable(godzinaInput));
+        godzinaInput.click();
+        this.ClearField(godzinaInput);
+        godzinaInput.sendKeys(insertGodzina);
+    }
+
+    public void directConnections(){
+
+        wait = new WebDriverWait(driver, 10);
+        wait.until(elementToBeClickable(directConnections));
+        directConnections.click();
+    }
+
+    public void moreSearchOptions(){
+
+        wait = new WebDriverWait(driver, 10);
+        wait.until(elementToBeClickable(moreSearchOptions));
+        moreSearchOptions.click();
+    }
+
+    public void closeSearchOptions(){
+
+        wait = new WebDriverWait(driver, 10);
+        wait.until(elementToBeClickable(closeSearchOptions));
+        closeSearchOptions.click();
     }
 }
